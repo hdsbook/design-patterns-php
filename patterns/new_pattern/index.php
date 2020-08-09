72b86f4bc69aac429a761913caf4f8bdae85c3d6
zang <myginnygo@gmail.com>
2020-08-09 21:13:44 +0800

    add bridge pattern

--- patterns/new_pattern/index.php
+++ patterns/new_pattern/index.php
@@ -9,3 +9,9 @@ $new_pattern = new Newpattern();
 /**
  * Newpattern
  */
+?>
+<pre>
+    Newpattern 或稱 "模式"
+</pre>
+
+<a href="https://www.notion.so/Newpattern-15218afb7e1a4e6082e8253cc1bd2bd9" target="_blank">更多…</a>
