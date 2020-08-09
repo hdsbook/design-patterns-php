b1f40df987a25bf0c6a2374d3565e126c2b34721
zang <myginnygo@gmail.com>
2020-08-09 15:58:22 +0800

    加上中文說明

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <h1>23種設計模式：</h1>
 
 <ul>
-    <li><a href="patterns/facade">Facade</a></li>
-    <li><a href="patterns/adapter">Adapter</a></li>
-    <li><a href="patterns/strategy">Strategy</a></li>
+    <li><a href="patterns/facade">Facade (外表模式)</a></li>
+    <li><a href="patterns/adapter">Adapter (適配器模式)</a></li>
+    <li><a href="patterns/strategy">Strategy (策略模式)</a></li>
     <li><a href="patterns/bridge">Bridge</a></li>
     <li></li>
     <li></li>
