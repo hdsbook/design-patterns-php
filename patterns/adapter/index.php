bbe5164b80ed09f3861681ac97ae056a533d1c10
zang <myginnygo@gmail.com>
2020-08-09 14:35:21 +0800

    模式說明文字用 pre tag 呈現

--- patterns/adapter/index.php
+++ patterns/adapter/index.php
@@ -15,12 +15,13 @@ $square->draw();
 $ball = new Ball();
 $ball->draw();
 
-/**
- * adapter 中文有適配器、改編者、轉接器的意思，或稱 "適配器模式"
- *
- * Adapter 將一個類別的介面轉換成另一個介面。使原本由於介面不相容而不能一起工作的類別可以一起工作。
- *
- * Ball 是 Ball3d 的 adapter
- * Ball3d 衍生自 Shape3dInterface
- * Ball 衍生自 ShapeInterface，讓 Ball3d 可以為 ShapeInterface 所用，並維持 ShapeInterface 的多型
- */
+?>
+<pre>
+    Adapter 或稱 "適配器模式"
+    adapter 中文有適配器、改編者、轉接器的意思，
+
+    Adapter 將一個類別的介面轉換成另一個介面。使原本由於介面不相容而不能一起工作的類別可以一起工作。
+    Ball 是 Ball3d 的 adapter，Ball3d 是 Ball 的 adptee
+    Ball3d 衍生自 Shape3dInterface
+    Ball 衍生自 ShapeInterface，讓 Ball3d 可以為 ShapeInterface 所用，並維持 ShapeInterface 的多型
+</pre>
--- patterns/facade/index.php
+++ patterns/facade/index.php
@@ -7,8 +7,10 @@ use facade\Facade;
 $facade = new Facade();
 $facade->doSomeThing();
 
-/**
- * facade 中文有外表、浮面、表面的意思，或稱 "外觀模式"
- *
- * Facade 模式定義了一個更高層的介面(為子系統中的一組介面定義一個統一介面)，使子系統更容易使用。
- */
+?>
+<pre>
+    Facade 或稱 "外觀模式"
+    facade 中文有外表、浮面、表面的意思
+
+    Facade 模式定義了一個更高層的介面(為子系統中的一組介面定義一個統一介面)，使子系統更容易使用。
+</pre>
