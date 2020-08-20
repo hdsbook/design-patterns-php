26d67808ac8c745e84277e1edb25860bc5e3d8c1
zang <myginnygo@gmail.com>
2020-08-20 23:01:03 +0800

    add observer pattern

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <li><a href="patterns/bridge">Bridge (橋接模式)</a></li>
     <li><a href="patterns/abstractFactory">Abstract Factory (抽象工廠模式)</a></li>
     <li><a href="patterns/decorator">Decorator (裝飾模式)</a></li>
-    <li></li>
+    <li><a href="patterns/observer">Observer (觀察者)</a></li>
     <li></li>
     <li></li>
     <li></li>
