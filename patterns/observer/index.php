1f8970ec70ee74cb6db08ea3ceeaf3857736aa55
zang <myginnygo@gmail.com>
2020-08-20 23:12:44 +0800

    modify observer description

--- patterns/observer/index.php
+++ patterns/observer/index.php
@@ -30,9 +30,14 @@ $observable->notify();
 
 <pre>
     Observer 或稱 "模式"
-    定義物件間的一種一對多的相依關係，當一個物件的狀態發生改變時，所有相依於它的物件都將得到通知並自動更新
+    定義物件間的一種一對多的相依關係
+    當一個物件的狀態發生改變時，所有相依於它的物件都將得到通知並自動更新
+
     Subject 在所監視的事件發生時通知 Observer
     Observer 向 Subject 註冊，在得到通知時從 Subject 獲取資訊
+
+    將通知者與被通知者解耦
+    subject: 主題; observer: 觀察者
 
     php 的標準庫中，已經定義了 SplObserver 和 SplSubject 兩個 Interface 來快速規範 observer 模式的介面
 
