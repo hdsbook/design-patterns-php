1b9f87c80321aaee6b62aa1204ea5301ce59f714
zang <myginnygo@gmail.com>
2020-09-06 21:58:29 +0800

    改變資料夾結構

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <h1>23種設計模式：</h1>
 
 <ol>
-    <li><a href="patterns/facade">Facade (外表模式)</a></li>
-    <li><a href="patterns/adapter">Adapter (適配器模式)</a></li>
-    <li><a href="patterns/strategy">Strategy (策略模式)</a></li>
-    <li><a href="patterns/bridge">Bridge (橋接模式)</a></li>
-    <li><a href="patterns/abstractFactory">Abstract Factory (抽象工廠模式)</a></li>
-    <li><a href="patterns/decorator">Decorator (裝飾模式)</a></li>
-    <li><a href="patterns/observer">Observer (觀察者模式)</a></li>
-    <li><a href="patterns/templateMethod">Template Method (樣板方法模式)</a></li>
-    <li><a href="patterns/singleton">Singleton Method (單例模式)</a></li>
+    <li><a href="facade">Facade (外表模式)</a></li>
+    <li><a href="adapter">Adapter (適配器模式)</a></li>
+    <li><a href="strategy">Strategy (策略模式)</a></li>
+    <li><a href="bridge">Bridge (橋接模式)</a></li>
+    <li><a href="abstractFactory">Abstract Factory (抽象工廠模式)</a></li>
+    <li><a href="decorator">Decorator (裝飾模式)</a></li>
+    <li><a href="observer">Observer (觀察者模式)</a></li>
+    <li><a href="templateMethod">Template Method (樣板方法模式)</a></li>
+    <li><a href="singleton">Singleton Method (單例模式)</a></li>
 </ol>
